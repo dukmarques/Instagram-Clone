8fb287c6fac7fba94b83d95fe2399ecbdc24fb06
Eduardo Marques <33849830+dukmarques@users.noreply.github.com>
2019-01-04 07:40:50 -0300

    Alteração de icone e inicio do cadastro de usuário

--- app/src/main/java/com/instagramclone/eduardo/instagramclone/activity/CadastroActivity.java
+++ app/src/main/java/com/instagramclone/eduardo/instagramclone/activity/CadastroActivity.java
@@ -2,14 +2,70 @@ package com.instagramclone.eduardo.instagramclone.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.instagramclone.eduardo.instagramclone.R;
+import com.instagramclone.eduardo.instagramclone.model.Usuario;
 
 public class CadastroActivity extends AppCompatActivity {
+    private EditText campoNome, campoEmail, campoSenha;
+    private Button botaoCadastrar;
+    private ProgressBar progressBar;
+
+    private Usuario usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        inicializarComponentes();
+
+        //Cadastrar Usuário
+        progressBar.setVisibility(View.GONE);
+        botaoCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                String textoNome = campoNome.getText().toString();
+                String textoEmail = campoEmail.getText().toString();
+                String textoSenha = campoSenha.getText().toString();
+
+                if (!textoNome.isEmpty()){
+                    if (!textoEmail.isEmpty()){
+                        if (!textoSenha.isEmpty()){
+                            usuario = new Usuario();
+                            usuario.setNome(textoNome);
+                            usuario.setEmail(textoEmail);
+                            usuario.setSenha(textoSenha);
+
+                            cadastrarUsuário(usuario);
+                        }else{
+                            Toast.makeText(CadastroActivity.this, "Preencha a senha", Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(CadastroActivity.this, "Preencha o e-mail", Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(CadastroActivity.this, "Preencha o nome", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void cadastrarUsuário(Usuario usuario){
+
+    }
+
+    private void inicializarComponentes(){
+        campoNome = findViewById(R.id.editCadastroNome);
+        campoEmail = findViewById(R.id.editCadastroEmail);
+        campoSenha = findViewById(R.id.editCadastroSenha);
+        botaoCadastrar = findViewById(R.id.buttonCadastrar);
+        progressBar = findViewById(R.id.progressCadastro);
     }
 }
