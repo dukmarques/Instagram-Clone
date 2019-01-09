05f6cce02a6879e048348a8cee0764b3917cfa3c
Eduardo Marques <33849830+dukmarques@users.noreply.github.com>
2019-01-09 02:18:10 -0300

    Tela de alteração de dados

--- app/src/main/java/com/instagramclone/eduardo/instagramclone/fragment/PerfilFragment.java
+++ app/src/main/java/com/instagramclone/eduardo/instagramclone/fragment/PerfilFragment.java
@@ -1,19 +1,31 @@
 package com.instagramclone.eduardo.instagramclone.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.instagramclone.eduardo.instagramclone.R;
+import com.instagramclone.eduardo.instagramclone.activity.EditarPerfilActivity;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PerfilFragment extends Fragment {
-
+    private ProgressBar progressBar;
+    private CircleImageView imagePerfil;
+    private GridView gridViewPerfil;
+    private TextView textPublicacoes, textSeguidores, textSeguindo;
+    private Button buttonEditarPerfil;
 
     public PerfilFragment() {
         // Required empty public constructor
@@ -24,7 +36,27 @@ public class PerfilFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil, container, false);
+        View view = inflater.inflate(R.layout.fragment_perfil, container, false);
+
+        //Configurações dos componentes
+        gridViewPerfil = view.findViewById(R.id.gridViewPerfil);
+        progressBar = view.findViewById(R.id.progressBarPerfil);
+        imagePerfil = view.findViewById(R.id.imagePerfil);
+        textPublicacoes = view.findViewById(R.id.textPublicacoes);
+        textSeguidores = view.findViewById(R.id.textSeguidores);
+        textSeguindo = view.findViewById(R.id.textSeguindo);
+        buttonEditarPerfil = view.findViewById(R.id.buttonEditarPerfil);
+
+        //Abre edição do perfil
+        buttonEditarPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), EditarPerfilActivity.class);
+                startActivity(i);
+            }
+        });
+
+        return view;
     }
 
 }
