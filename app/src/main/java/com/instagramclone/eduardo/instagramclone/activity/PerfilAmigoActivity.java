9d953a3d3bdb08fed5f58187cd1e1508f42ea4d6
Eduardo Marques <33849830+dukmarques@users.noreply.github.com>
2019-01-09 17:29:38 -0300

    Exibição dos dados na tela de visualização de perfil de usuários

--- app/src/main/java/com/instagramclone/eduardo/instagramclone/activity/PerfilAmigoActivity.java
+++ app/src/main/java/com/instagramclone/eduardo/instagramclone/activity/PerfilAmigoActivity.java
@@ -1,25 +1,44 @@
 package com.instagramclone.eduardo.instagramclone.activity;
 
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.instagramclone.eduardo.instagramclone.R;
+import com.instagramclone.eduardo.instagramclone.helper.ConfiguracaoFirebase;
 import com.instagramclone.eduardo.instagramclone.model.Usuario;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class PerfilAmigoActivity extends AppCompatActivity {
     private Usuario usuarioSelecionado;
     private Button buttonAcaoPerfil;
+    private CircleImageView imagePerfil;
+    private TextView textPublicacoes, textSeguidores, textSeguindo;
+
+    private DatabaseReference usuariosRef;
+    private DatabaseReference usuarioAmigoRef;
+    private ValueEventListener valueEventListenerPerfilAmigo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_amigo);
 
+        //Configurações iniciais
+        usuariosRef = ConfiguracaoFirebase.getFirebase().child("usuarios");
+
         //Inicializar Componentes
-        buttonAcaoPerfil = findViewById(R.id.buttonAcaoPerfil);
-        buttonAcaoPerfil.setText("Seguir");
+        inicializarComponentes();
 
         //Configurar toolbar
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
@@ -36,7 +55,63 @@ public class PerfilAmigoActivity extends AppCompatActivity {
 
             //Configura nome do usuário na toolbar
             getSupportActionBar().setTitle(usuarioSelecionado.getNome());
+
+            //Recuperar foto do usuário
+            String caminhoFoto = usuarioSelecionado.getCaminhoFoto();
+            if (caminhoFoto != null){
+                Uri url = Uri.parse(caminhoFoto);
+                Glide.with(PerfilAmigoActivity.this)
+                        .load(url)
+                        .into(imagePerfil);
+            }
         }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        recuperarDadosPerfilAmigo();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        usuarioAmigoRef.removeEventListener(valueEventListenerPerfilAmigo);
+    }
+
+    private void recuperarDadosPerfilAmigo(){
+        usuarioAmigoRef = usuariosRef.child(usuarioSelecionado.getId());
+        valueEventListenerPerfilAmigo = usuarioAmigoRef.addValueEventListener(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        Usuario usuario = dataSnapshot.getValue(Usuario.class);
+
+                        String postagens = String.valueOf(usuario.getPostagens());
+                        String seguindo = String.valueOf(usuario.getSeguindo());
+                        String seguidores = String.valueOf(usuario.getSeguidores());
+
+                        //Configura valores recuperados
+                        textPublicacoes.setText(postagens);
+                        textSeguidores.setText(seguidores);
+                        textSeguindo.setText(seguindo);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                }
+        );
+    }
+
+    private void inicializarComponentes(){
+        buttonAcaoPerfil = findViewById(R.id.buttonAcaoPerfil);
+        buttonAcaoPerfil.setText("Seguir");
+        imagePerfil = findViewById(R.id.imagePerfil);
+        textPublicacoes = findViewById(R.id.textPublicacoes);
+        textSeguidores = findViewById(R.id.textSeguidores);
+        textSeguindo = findViewById(R.id.textSeguindo);
     }
 
     @Override
--- app/src/main/java/com/instagramclone/eduardo/instagramclone/model/Usuario.java
+++ app/src/main/java/com/instagramclone/eduardo/instagramclone/model/Usuario.java
@@ -14,6 +14,9 @@ public class Usuario implements Serializable {
     private String email;
     private String senha;
     private String caminhoFoto;
+    private int seguidores = 0;
+    private int seguindo = 0;
+    private int postagens = 0;
 
     public Usuario() {
     }
@@ -39,6 +42,9 @@ public class Usuario implements Serializable {
         usuarioMap.put("nome", getNome());
         usuarioMap.put("id", getId());
         usuarioMap.put("caminhoFoto", getCaminhoFoto());
+        usuarioMap.put("seguidores", getSeguidores());
+        usuarioMap.put("seguindo", getSeguindo());
+        usuarioMap.put("postagens", getPostagens());
 
         return usuarioMap;
     }
@@ -82,5 +88,29 @@ public class Usuario implements Serializable {
 
     public void setCaminhoFoto(String caminhoFoto) {
         this.caminhoFoto = caminhoFoto;
+    }
+
+    public int getSeguidores() {
+        return seguidores;
+    }
+
+    public void setSeguidores(int seguidores) {
+        this.seguidores = seguidores;
+    }
+
+    public int getSeguindo() {
+        return seguindo;
+    }
+
+    public void setSeguindo(int seguindo) {
+        this.seguindo = seguindo;
+    }
+
+    public int getPostagens() {
+        return postagens;
+    }
+
+    public void setPostagens(int postagens) {
+        this.postagens = postagens;
     }
 }
