7ddf0e98bed3d42b0c680180975b3f7f4a758b5a
Eduardo Marques <33849830+dukmarques@users.noreply.github.com>
2019-01-09 15:24:44 -0300

    pesquisa de usuários

--- app/src/main/java/com/instagramclone/eduardo/instagramclone/activity/EditarPerfilActivity.java
+++ app/src/main/java/com/instagramclone/eduardo/instagramclone/activity/EditarPerfilActivity.java
@@ -67,7 +67,7 @@ public class EditarPerfilActivity extends AppCompatActivity {
 
         //Recuperar os dados do usuário
         final FirebaseUser usuarioPerfil = UsuarioFirebase.getUsuarioAtual();
-        editNomePerfil.setText(usuarioPerfil.getDisplayName());
+        editNomePerfil.setText(usuarioPerfil.getDisplayName().toUpperCase());
         editEmailPerfil.setText(usuarioPerfil.getEmail());
 
         Uri url = usuarioPerfil.getPhotoUrl();
--- app/src/main/java/com/instagramclone/eduardo/instagramclone/fragment/PesquisaFragment.java
+++ app/src/main/java/com/instagramclone/eduardo/instagramclone/fragment/PesquisaFragment.java
@@ -2,6 +2,7 @@ package com.instagramclone.eduardo.instagramclone.fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
@@ -10,7 +11,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.instagramclone.eduardo.instagramclone.R;
+import com.instagramclone.eduardo.instagramclone.helper.ConfiguracaoFirebase;
+import com.instagramclone.eduardo.instagramclone.model.Usuario;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,6 +29,9 @@ import com.instagramclone.eduardo.instagramclone.R;
 public class PesquisaFragment extends Fragment {
     private SearchView searchViewPesquisa;
     private RecyclerView recyclerPesquisa;
+
+    private List<Usuario> listaUsuarios;
+    private DatabaseReference usuariosRef;
 
     public PesquisaFragment() {
         // Required empty public constructor
@@ -32,6 +46,10 @@ public class PesquisaFragment extends Fragment {
         searchViewPesquisa = view.findViewById(R.id.searchViewPesquisa);
         recyclerPesquisa = view.findViewById(R.id.recyclerPesquisa);
 
+        //Configurações iniciais
+        listaUsuarios = new ArrayList<>();
+        usuariosRef = ConfiguracaoFirebase.getFirebase().child("usuarios");
+
         //Configura searchview
         searchViewPesquisa.setQueryHint("Buscar Usuários");
         searchViewPesquisa.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
@@ -42,11 +60,41 @@ public class PesquisaFragment extends Fragment {
 
             @Override
             public boolean onQueryTextChange(String s) {
-                
+                String textoDigitado = s.toUpperCase();
+                pesquisarUsuarios(textoDigitado);
                 return true;
             }
         });
 
         return view;
+    }
+
+    private void pesquisarUsuarios(String texto){
+        //Limpar lista
+        listaUsuarios.clear();
+
+        //Pesquisa usuários cado tenha texto na pesquisa
+        if (texto.length() > 0){
+            Query query = usuariosRef.orderByChild("nome")
+                    .startAt(texto)
+                    .endAt(texto + "\uf8ff");
+
+            query.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    for (DataSnapshot ds : dataSnapshot.getChildren()){
+                        listaUsuarios.add(ds.getValue(Usuario.class));
+                    }
+
+                    int total = listaUsuarios.size();
+                    Log.i("totalUsuarios", "total: " + total);
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+        }
     }
 }
