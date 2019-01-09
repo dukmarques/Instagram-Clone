b4e35ef1c7d6192140f059c0eff4b7a92096c470
Eduardo Marques <33849830+dukmarques@users.noreply.github.com>
2019-01-09 14:57:03 -0300

    Tela de pesquisa e SearchView

--- app/src/main/java/com/instagramclone/eduardo/instagramclone/fragment/PesquisaFragment.java
+++ app/src/main/java/com/instagramclone/eduardo/instagramclone/fragment/PesquisaFragment.java
@@ -3,6 +3,9 @@ package com.instagramclone.eduardo.instagramclone.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,18 +16,37 @@ import com.instagramclone.eduardo.instagramclone.R;
  * A simple {@link Fragment} subclass.
  */
 public class PesquisaFragment extends Fragment {
-
+    private SearchView searchViewPesquisa;
+    private RecyclerView recyclerPesquisa;
 
     public PesquisaFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pesquisa, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_pesquisa, container, false);
 
+        searchViewPesquisa = view.findViewById(R.id.searchViewPesquisa);
+        recyclerPesquisa = view.findViewById(R.id.recyclerPesquisa);
+
+        //Configura searchview
+        searchViewPesquisa.setQueryHint("Buscar Usuários");
+        searchViewPesquisa.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                
+                return true;
+            }
+        });
+
+        return view;
+    }
 }
