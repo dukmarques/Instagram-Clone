5343ae358e5942ca12d7ce4091df387b1da472ce
Eduardo Marques <33849830+dukmarques@users.noreply.github.com>
2019-01-10 03:00:17 -0300

    Ajustando tela de perfil

--- app/src/main/java/com/instagramclone/eduardo/instagramclone/fragment/PerfilFragment.java
+++ app/src/main/java/com/instagramclone/eduardo/instagramclone/fragment/PerfilFragment.java
@@ -2,7 +2,9 @@ package com.instagramclone.eduardo.instagramclone.fragment;
 
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,8 +14,26 @@ import android.widget.GridView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.instagramclone.eduardo.instagramclone.R;
 import com.instagramclone.eduardo.instagramclone.activity.EditarPerfilActivity;
+import com.instagramclone.eduardo.instagramclone.activity.PerfilAmigoActivity;
+import com.instagramclone.eduardo.instagramclone.adapter.AdapterGrid;
+import com.instagramclone.eduardo.instagramclone.helper.ConfiguracaoFirebase;
+import com.instagramclone.eduardo.instagramclone.helper.UsuarioFirebase;
+import com.instagramclone.eduardo.instagramclone.model.Postagem;
+import com.instagramclone.eduardo.instagramclone.model.Usuario;
+import com.nostra13.universalimageloader.cache.disc.naming.HashCodeFileNameGenerator;
+import com.nostra13.universalimageloader.cache.memory.impl.LruMemoryCache;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -26,6 +46,14 @@ public class PerfilFragment extends Fragment {
     private GridView gridViewPerfil;
     private TextView textPublicacoes, textSeguidores, textSeguindo;
     private Button buttonAcaoPerfil;
+    private Usuario usuarioLogado;
+
+    private DatabaseReference firebaseRef;
+    private DatabaseReference usuariosRef;
+    private DatabaseReference usuarioLogadoRef;
+    private ValueEventListener valueEventListenerPerfil;
+    private DatabaseReference postagensUsuarioRef;
+    private AdapterGrid adapterGrid;
 
     public PerfilFragment() {
         // Required empty public constructor
@@ -38,14 +66,27 @@ public class PerfilFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_perfil, container, false);
 
+        //Configurações iniciais
+        usuarioLogado = UsuarioFirebase.getDadosUsuarioLogado();
+        firebaseRef = ConfiguracaoFirebase.getFirebase();
+        usuariosRef = firebaseRef.child("usuarios");
+
+        //Configurar referencia para postangens do usuário
+        postagensUsuarioRef = ConfiguracaoFirebase.getFirebase()
+                .child("postagens")
+                .child(usuarioLogado.getId());
+
         //Configurações dos componentes
-        gridViewPerfil = view.findViewById(R.id.gridViewPerfil);
-        progressBar = view.findViewById(R.id.progressBarPerfil);
-        imagePerfil = view.findViewById(R.id.imagePerfil);
-        textPublicacoes = view.findViewById(R.id.textPublicacoes);
-        textSeguidores = view.findViewById(R.id.textSeguidores);
-        textSeguindo = view.findViewById(R.id.textSeguindo);
-        buttonAcaoPerfil = view.findViewById(R.id.buttonAcaoPerfil);
+        inicializarComponentes(view);
+
+        //Recuperar foto do usuário
+        String caminhoFoto = usuarioLogado.getCaminhoFoto();
+        if (caminhoFoto != null){
+            Uri url = Uri.parse(caminhoFoto);
+            Glide.with(getActivity())
+                    .load(url)
+                    .into(imagePerfil);
+        }
 
         //Abre edição do perfil
         buttonAcaoPerfil.setOnClickListener(new View.OnClickListener() {
@@ -56,7 +97,109 @@ public class PerfilFragment extends Fragment {
             }
         });
 
+        //Inicializar Image Loader
+        incializarImageLoader();
+
+        //Carregar as fotos das postagens do usuário visitado
+        carregarFotosPostagem();
+
         return view;
     }
 
+    public void carregarFotosPostagem(){
+        //Configurar o tamanho do grid
+        int tamanhoGrid = getResources().getDisplayMetrics().widthPixels;
+        int tamanhoImagem = tamanhoGrid / 3;
+        gridViewPerfil.setColumnWidth(tamanhoImagem);
+
+        //Recupera as fotos postadas pelo usuário
+        postagensUsuarioRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                List<String> urlFotos = new ArrayList<>();
+
+                for (DataSnapshot ds : dataSnapshot.getChildren()){
+                    Postagem postagem = ds.getValue(Postagem.class);
+                    urlFotos.add(postagem.getCaminhoFoto());
+                }
+
+                int qtdPostagem = urlFotos.size();
+                textPublicacoes.setText(String.valueOf(qtdPostagem));
+
+                //Configurar adapter
+                adapterGrid = new AdapterGrid(getActivity(), R.layout.grid_postagem, urlFotos);
+                gridViewPerfil.setAdapter(adapterGrid);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    /**
+     * Instancia a UniversalImageLoader
+     */
+    public void incializarImageLoader(){
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration
+                .Builder(getActivity())
+                .memoryCache(new LruMemoryCache(2 * 1024 * 1024))
+                .memoryCacheSize(2 * 1024 * 1024)
+                .diskCacheSize(50 * 1024 * 1024)
+                .diskCacheFileCount(100)
+                .diskCacheFileNameGenerator(new HashCodeFileNameGenerator())
+                .build();
+        ImageLoader.getInstance().init(config);
+    }
+
+    private void inicializarComponentes(View view){
+        gridViewPerfil = view.findViewById(R.id.gridViewPerfil);
+        progressBar = view.findViewById(R.id.progressBarPerfil);
+        imagePerfil = view.findViewById(R.id.imagePerfil);
+        textPublicacoes = view.findViewById(R.id.textPublicacoes);
+        textSeguidores = view.findViewById(R.id.textSeguidores);
+        textSeguindo = view.findViewById(R.id.textSeguindo);
+        buttonAcaoPerfil = view.findViewById(R.id.buttonAcaoPerfil);
+    }
+
+    private void recuperarDadosUsuarioLogado(){
+        usuarioLogadoRef = usuariosRef.child(usuarioLogado.getId());
+        valueEventListenerPerfil = usuarioLogadoRef.addValueEventListener(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        Usuario usuario = dataSnapshot.getValue(Usuario.class);
+
+                        //String postagens = String.valueOf(usuario.getPostagens());
+                        String seguindo = String.valueOf(usuario.getSeguindo());
+                        String seguidores = String.valueOf(usuario.getSeguidores());
+
+                        //Configura valores recuperados
+                        //textPublicacoes.setText(postagens);
+                        textSeguidores.setText(seguidores);
+                        textSeguindo.setText(seguindo);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                }
+        );
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        //Recuperar dados usuario logado
+        recuperarDadosUsuarioLogado();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        usuarioLogadoRef.removeEventListener(valueEventListenerPerfil);
+    }
 }
