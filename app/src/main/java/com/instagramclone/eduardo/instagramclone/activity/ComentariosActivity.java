8a18fd8093ab32c21766e8095cd4afe311f10fcd
Eduardo Marques <33849830+dukmarques@users.noreply.github.com>
2019-01-10 23:47:41 -0300

    Salvando comentários

--- app/src/main/java/com/instagramclone/eduardo/instagramclone/activity/ComentariosActivity.java
+++ app/src/main/java/com/instagramclone/eduardo/instagramclone/activity/ComentariosActivity.java
@@ -3,15 +3,31 @@ package com.instagramclone.eduardo.instagramclone.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.instagramclone.eduardo.instagramclone.R;
+import com.instagramclone.eduardo.instagramclone.helper.UsuarioFirebase;
+import com.instagramclone.eduardo.instagramclone.model.Comentario;
+import com.instagramclone.eduardo.instagramclone.model.Usuario;
 
 public class ComentariosActivity extends AppCompatActivity {
+    private EditText editComentario;
+
+    private String idPostagem;
+    private Usuario usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comentarios);
+
+        //Inicializar Componentes
+        editComentario = findViewById(R.id.editComentario);
+
+        //Configurações iniciais
+        usuario = UsuarioFirebase.getDadosUsuarioLogado();
 
         //Configurar toolbar
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
@@ -20,5 +36,39 @@ public class ComentariosActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_black_24dp);
+
+        //Recupera id da postagem
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null){
+            idPostagem = bundle.getString("idPostagem");
+        }
+    }
+
+    public void salvarComentário(View view){
+        String textoComentario = editComentario.getText().toString();
+        if (textoComentario != null && !textoComentario.equals("")){
+            Comentario comentario = new Comentario();
+            comentario.setIdPostagem(idPostagem);
+            comentario.setIdUsuario(usuario.getId());
+            comentario.setNomeUsuario(usuario.getNome());
+            comentario.setCaminhoFoto(usuario.getCaminhoFoto());
+            comentario.setComentario(textoComentario);
+            
+            if (comentario.salvar()){
+                Toast.makeText(this, "Comentário salvo", Toast.LENGTH_SHORT).show();
+            }
+
+        }else{
+            Toast.makeText(this, "Escreva um comentário", Toast.LENGTH_SHORT).show();
+        }
+
+        //Limpar comentário digitado
+        editComentario.setText("");
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return false;
     }
 }
